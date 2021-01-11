2f9009df4bf623af69066ec7502dc3c7aea5710c
Mahmood28 <mahmoodalwatani@gmail.com>
2021-01-11 13:21:42 +0300

    Solved Array-while task!

--- arrayFunctions.js
+++ arrayFunctions.js
@@ -9,7 +9,7 @@
  * isArrayLengthOdd([1, 2, 3, 4]) -> flase
  */
 function isArrayLengthOdd(numbers) {
-  // Your code here
+  return (numbers.length %2 === 1)
 }
 
 /**
@@ -23,7 +23,7 @@ function isArrayLengthOdd(numbers) {
  * isArrayLengthEven([1, 2, 3, 4]) -> true
  */
 function isArrayLengthEven(numbers) {
-  // Your code here
+return (numbers.length %2 === 0)
 }
 
 /**
@@ -35,7 +35,8 @@ function isArrayLengthEven(numbers) {
  * addLailaToArray(["Mshary", "Hasan"]) -> ["Mshary", "Hasan", "Laila"]
  */
 function addLailaToArray(instructors) {
-  // Your code here
+  instructors.push("Laila");
+  return instructors
 }
 
 /**
@@ -47,7 +48,7 @@ function addLailaToArray(instructors) {
  * eliminateTeam(["Brazil", "Germany", "Italy"]) -> "Italy"
  */
 function eliminateTeam(teams) {
-  // Your code here
+  return teams.pop()
 }
 
 /**
@@ -61,7 +62,9 @@ function eliminateTeam(teams) {
  * secondHalfOfArrayIfItIsEven(["apple", "orange", "banana", "kiwi", "blueberry"]) -> []
  */
 function secondHalfOfArrayIfItIsEven(fruits) {
-  // Your code here
+  if (fruits.length % 2 === 0) return fruits.slice(fruits.length/2)
+  else return []
+
 }
 
 /**
@@ -79,9 +82,9 @@ function secondHalfOfArrayIfItIsEven(fruits) {
  * - Use string method .endsWith()
  */
 function youGottaCalmDown(shout) {
-  // Your code here
+  while (shout.endsWith("!!")) { shout = shout.slice(0, -1) }
+  return shout
 }
-
 module.exports = {
   isArrayLengthOdd,
   isArrayLengthEven,
